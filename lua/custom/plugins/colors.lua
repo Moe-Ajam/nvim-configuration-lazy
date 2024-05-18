4dfda7fe071a247a4f71a5ad005d1881f7d596e8
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-05-18 23:08:06 +0300

    Got rid of the function guidelines finally, and swiched to tokyo night font

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-  color = color or 'rose-pine'
+  color = color or 'tokyonight'
   vim.cmd.colorscheme(color)
 
   -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
@@ -9,21 +9,23 @@ end
 return {
   {
     'folke/tokyonight.nvim',
+    priority = 1000,
     config = function()
       require('tokyonight').setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        transparent = true, -- Enable this to disable setting the background color
+        --style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        --transparent = true, -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
           -- Style to be applied to different syntax groups
           -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = false },
-          keywords = { italic = false },
+          -- comments = { italic = false },
+          -- keywords = { italic = false },
           -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = 'dark', -- style for sidebars, see below
-          floats = 'dark', -- style for floating windows
+          -- sidebars = 'dark', -- style for sidebars, see below
+          -- floats = 'dark', -- style for floating windows
+          bold = false,
         },
       }
     end,
@@ -31,17 +33,18 @@ return {
 
   {
     'rose-pine/neovim',
+    -- priority = 1000,
     name = 'rose-pine',
     config = function()
       require('rose-pine').setup {
-        disable_background = true,
-        styles = {
-          italic = false,
-          bold = false,
-        },
+        --disable_background = true,
+        -- styles = {
+        --   italic = false,
+        --   bold = false,
+        -- },
       }
 
-      vim.cmd 'colorscheme rose-pine'
+      vim.cmd 'colorscheme tokyonight'
 
       ColorMyPencils()
     end,
