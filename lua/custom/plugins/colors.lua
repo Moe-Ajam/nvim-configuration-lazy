5493f9a200d248ecaf4ad8f680df9c45904663ee
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-07-25 22:11:26 +0300

    new color scheme applied with rose pine main

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -52,9 +52,35 @@ return {
           -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
       }
+    end,
+  },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        variant = 'auto', -- auto, main, moon, or dawn
+        dark_variant = 'main', -- main, moon, or dawn
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
 
-      -- setup must be called before loading
-      vim.cmd.colorscheme 'catppuccin'
+        enable = {
+          terminal = true,
+          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+          migrations = true, -- Handle deprecated options automatically
+        },
+
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = false,
+        },
+      }
+
+      vim.cmd 'colorscheme rose-pine'
+      -- vim.cmd("colorscheme rose-pine-main")
+      -- vim.cmd 'colorscheme rose-pine-moon'
+      -- vim.cmd("colorscheme rose-pine-dawn")
     end,
   },
 }
