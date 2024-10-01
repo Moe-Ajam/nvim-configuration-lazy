9990d37973af14a612095cecc2add1f24271f8a7
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-10-01 21:51:54 +0300

    fixed the treesitter to include go, also nehanced the configuration to include all requried fields

--- lua/moeajam/plugins/treesitter.lua
+++ lua/moeajam/plugins/treesitter.lua
@@ -1,41 +1,46 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-      },
-    })
-  end,
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			modules = {},
+			sync_install = true,
+			auto_install = true,
+			ignore_install = {},
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"go",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"vimdoc",
+				"c",
+			},
+		})
+	end,
 }
