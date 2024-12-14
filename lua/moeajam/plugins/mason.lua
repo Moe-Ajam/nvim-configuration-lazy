50428f559db3ebff4071004bf400d6acec1b910e
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-12-14 14:37:17 +0300

    added java language support

--- lua/moeajam/plugins/mason.lua
+++ lua/moeajam/plugins/mason.lua
@@ -37,6 +37,7 @@ return {
 				"prismals",
 				"pyright",
 				"gopls",
+				"jdtls",
 			},
 		})
 		mason_tool_installer.setup({
--- lua/moeajam/plugins/treesitter.lua
+++ lua/moeajam/plugins/treesitter.lua
@@ -40,6 +40,7 @@ return {
 				"query",
 				"vimdoc",
 				"c",
+				"java",
 			},
 		})
 	end,
